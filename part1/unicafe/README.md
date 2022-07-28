dc5504d26f657439067aa11b0e9af7d6469cd5d1
daydy225 <dabelydadi@gmail.com>
2022-07-28 23:26:03 +0000

    unicafe step2

--- part1/unicafe/README.md
+++ part1/unicafe/README.md
@@ -3,3 +3,7 @@
 ## step1
 
 Implement a web application for collecting customer feedback where there are three options for feedback: good, neutral, and bad.
+
+## step2
+
+Expand the application so that it shows more statistics about the gathered feedback: the total number of collected feedback, the average score (good: 1, neutral: 0, bad: -1) and the percentage of positive feedback.
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,7 +4,7 @@ const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const Title = ({ title }) => <h2>{title}</h2>;
 
-const Statistic = ({ text, number }) => (
+const Statistics = ({ text, number }) => (
   <p>
     {text} {number}
   </p>
@@ -14,6 +14,21 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
+  let all = 0;
+  const arr = [good, neutral, bad];
+  arr.forEach((num) => (all += num));
+
+  const score = {
+    good: 1,
+    neutral: 0,
+    bad: -1,
+  };
+
+  let average =
+    (good * score.good + neutral * score.neutral + bad * score.bad) / all;
+
+  let positive = (good / all) * 100;
 
   const handleGoodClick = () => setGood(good + 1);
   const handleNeutralClick = () => setNeutral(neutral + 1);
@@ -26,9 +41,12 @@ const App = () => {
       <Button onClick={handleNeutralClick} text="Neutral" />
       <Button onClick={handleBadClick} text="Bad" />
       <Title title="statistics" />
-      <Statistic text="good" number={good} />
-      <Statistic text="Neutral" number={neutral} />
-      <Statistic text="bad" number={bad} />
+      <Statistics text="good" number={good} />
+      <Statistics text="Neutral" number={neutral} />
+      <Statistics text="bad" number={bad} />
+      <Statistics text="all" number={all} />
+      <Statistics text="average" number={average} />
+      <Statistics text="positive" number={`${positive} %`} />
     </div>
   );
 };
